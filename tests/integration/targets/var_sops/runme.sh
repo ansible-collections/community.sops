3b1d3c0d8ecde58bb1f7dcb7cbc9eb316a85fa12
Felix Fontein <felix@fontein.de>
2020-09-01 18:00:19 +0200

    Fix vars loading, and add simple tests.

---
+++ tests/integration/targets/var_sops/runme.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eux
+
+ANSIBLE_ROLES_PATH=.. ansible-playbook setup.yml
+
+if [ "$(which sops)" == "" ]; then
+    # sops was not installed
+    exit
+fi
+
+ANSIBLE_VARS_ENABLED=host_group_vars,community.sops.sops ansible-playbook test-1.yml -vvvv "$@"
