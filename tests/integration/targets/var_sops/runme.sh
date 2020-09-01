18c967ec9d7d4b77101a42a02b2a405e6848d376
Felix Fontein <felix@fontein.de>
2020-09-01 18:10:36 +0200

    Don't run tests with Ansible 2.9.

--- tests/integration/targets/var_sops/runme.sh
+++ tests/integration/targets/var_sops/runme.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -eux
 
+# Don't run this on Ansible 2.9
+if [ ansible-version | grep -F 'ansible 2.9.' ]; then
+    # Ansible 2.9 doesn't know about var plugins
+    exit
+fi
+
+# Install sops
 ANSIBLE_ROLES_PATH=.. ansible-playbook setup.yml
 
 if [ "$(which sops)" == "" ]; then
@@ -8,4 +15,5 @@ if [ "$(which sops)" == "" ]; then
     exit
 fi
 
+# First test
 ANSIBLE_VARS_ENABLED=host_group_vars,community.sops.sops ansible-playbook test-1.yml -v "$@"
