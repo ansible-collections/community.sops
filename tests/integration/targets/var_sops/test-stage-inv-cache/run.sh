d64e4e5d3f4c9c0fc3cc13cdc3c524d6c115adb8
Felix Fontein <felix@fontein.de>
2020-11-12 08:53:25 +0100

    Work around bug with implicit localhost, where vars plugins are called differently than for explicitly specified hosts.

--- tests/integration/targets/var_sops/test-stage-inv-cache/run.sh
+++ tests/integration/targets/var_sops/test-stage-inv-cache/run.sh
@@ -2,4 +2,4 @@
 set -e
 ANSIBLE_VARS_SOPS_PLUGIN_STAGE=inventory \
 ANSIBLE_VARS_SOPS_PLUGIN_CACHE=true \
-ansible-playbook playbook.yml -v "$@"
+ansible-playbook playbook.yml -i hosts -v "$@"
--- tests/integration/targets/var_sops/test-stage-task-no-cache/run.sh
+++ tests/integration/targets/var_sops/test-stage-task-no-cache/run.sh
@@ -2,4 +2,4 @@
 set -e
 ANSIBLE_VARS_SOPS_PLUGIN_STAGE=task \
 ANSIBLE_VARS_SOPS_PLUGIN_CACHE=false \
-ansible-playbook playbook.yml -v "$@"
+ansible-playbook playbook.yml -i hosts -v "$@"
