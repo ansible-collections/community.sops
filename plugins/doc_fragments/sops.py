7dccdeaef9f92732dacb383244063e16a3413078
Edoardo Tenani <endorama@users.noreply.github.com>
2021-01-14 16:47:50 +0100

    update version_added to 1.0.0

--- plugins/doc_fragments/sops.py
+++ plugins/doc_fragments/sops.py
@@ -15,31 +15,31 @@ options:
             - Path to the sops binary.
             - By default uses C(sops).
         type: path
-        version_added: 0.3.0
+        version_added: 1.0.0
     aws_profile:
         description:
             - The AWS profile to use for requests to AWS.
             - This corresponds to the sops C(--aws-profile) option.
         type: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     aws_access_key_id:
         description:
             - The AWS access key ID to use for requests to AWS.
             - Sets the environment variable C(AWS_ACCESS_KEY_ID) for the sops call.
         type: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     aws_secret_access_key:
         description:
             - The AWS secret access key to use for requests to AWS.
             - Sets the environment variable C(AWS_SECRET_ACCESS_KEY) for the sops call.
         type: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     aws_session_token:
         description:
             - The AWS session token to use for requests to AWS.
             - Sets the environment variable C(AWS_SESSION_TOKEN) for the sops call.
         type: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     config_path:
         description:
             - Path to the sops configuration file.
@@ -47,14 +47,14 @@ options:
               the file that is encrypted or decrypted.
             - This corresponds to the sops C(--config) option.
         type: path
-        version_added: 0.3.0
+        version_added: 1.0.0
     enable_local_keyservice:
         description:
             - Tell sops to use local key service.
             - This corresponds to the sops C(--enable-local-keyservice) option.
         type: bool
         default: false
-        version_added: 0.3.0
+        version_added: 1.0.0
     keyservice:
         description:
             - Specify key services to use next to the local one.
@@ -63,7 +63,7 @@ options:
             - This corresponds to the sops C(--keyservice) option.
         type: list
         elements: str
-        version_added: 0.3.0
+        version_added: 1.0.0
 '''
 
     ANSIBLE_VARIABLES = r'''
@@ -102,21 +102,21 @@ options:
             - This corresponds to the sops C(--kms) option.
         type: list
         elements: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     gcp_kms:
         description:
             - GCP KMS resource IDs to use.
             - This corresponds to the sops C(--gcp-kms) option.
         type: list
         elements: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     azure_kv:
         description:
             - Azure Key Vault URLs to use.
             - This corresponds to the sops C(--azure-kv) option.
         type: list
         elements: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     hc_vault_transit:
         description:
             - HashiCorp Vault key URIs to use.
@@ -124,20 +124,20 @@ options:
             - This corresponds to the sops C(--hc-vault-transit) option.
         type: list
         elements: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     pgp:
         description:
             - PGP fingerprints to use.
             - This corresponds to the sops C(--pgp) option.
         type: list
         elements: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     unencrypted_suffix:
         description:
             - Override the unencrypted key suffix.
             - This corresponds to the sops C(--unencrypted-suffix) option.
         type: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     encrypted_suffix:
         description:
             - Override the encrypted key suffix.
@@ -145,28 +145,28 @@ options:
               marked by I(unencrypted_suffix).
             - This corresponds to the sops C(--encrypted-suffix) option.
         type: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     unencrypted_regex:
         description:
             - Set the unencrypted key suffix.
             - When specified, only keys matching the regular expression will be left unencrypted.
             - This corresponds to the sops C(--unencrypted-regex) option.
         type: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     encrypted_regex:
         description:
             - Set the encrypted key suffix.
             - When specified, only keys matching the regular expression will be encrypted.
             - This corresponds to the sops C(--encrypted-regex) option.
         type: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     encryption_context:
         description:
             - List of KMS encryption context pairs of format C(key:value).
             - This corresponds to the sops C(--encryption-context) option.
         type: list
         elements: str
-        version_added: 0.3.0
+        version_added: 1.0.0
     shamir_secret_sharing_threshold:
         description:
             - The number of distinct keys required to retrieve the data key with
@@ -174,5 +174,5 @@ options:
             - If not set here and in the sops config file, will default to C(0).
             - This corresponds to the sops C(--shamir-secret-sharing-threshold) option.
         type: int
-        version_added: 0.3.0
+        version_added: 1.0.0
 '''
