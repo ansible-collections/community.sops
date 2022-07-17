305d5ddcea5460c62eb850b0625bc2fcbe9fa156
Felix Fontein <felix@fontein.de>
2022-07-17 12:26:18 +0200

    Add SPDX license identifiers, mention all licenses in galaxy.yml.

--- plugins/module_utils/sops.py
+++ plugins/module_utils/sops.py
@@ -1,5 +1,6 @@
 # Copyright (c), Edoardo Tenani <e.tenani@arduino.cc>, 2018-2020
-# Simplified BSD License (see simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
+# Simplified BSD License (see LICENSES/BSD-2-Clause.txt or https://opensource.org/licenses/BSD-2-Clause)
+# SPDX-License-Identifier: BSD-2-Clause
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
