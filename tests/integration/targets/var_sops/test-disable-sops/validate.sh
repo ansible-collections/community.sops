e3d322d7362e1159b65e60b59caba233e5afabc7
Florian Apolloner <florian@apolloner.eu>
2022-08-23 14:13:28 +0200

    Added configuration to temporarily disable the vars plugin (eg in AWX). (#114)

---
+++ tests/integration/targets/var_sops/test-disable-sops/validate.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Copyright (c) Ansible Project
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -eux
+
+if [ "$1" != 2 ]; then
+    exit 1
+fi
+
+grep -F "The error was: 'foo' is undefined" "$2"
